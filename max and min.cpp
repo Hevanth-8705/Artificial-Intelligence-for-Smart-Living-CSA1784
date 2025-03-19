c8947282adec3ea320984778876e110bdccdbd7c
Hevanth-8705 <bolligorla.hevanthkumar@gmail.com>
2025-03-19 09:52:19 +0530

    Add files via upload

---
+++ max and min.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int i,n=4,a[4]={12,34,56},max,min;
+	min=a[0],max=a[0];
+//	int *arr=a;
+	printf("enter the values");
+//	scanf("%d",&n);
+	for(i=1;i<n;i++)
+	{
+		if (arr[i]<min)
+		{
+		a[i]=min;
+	}
+			if (arr[i]>max)
+			{
+			arr[i]=max;
+				}
+				printf("%d\n%d\n",max,min);
+			}
+		}
